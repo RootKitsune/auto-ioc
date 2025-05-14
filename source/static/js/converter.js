84caec43d16f6168cfd01c0696cb8ed29f05144e
silchairmen <silmusic0113@gmail.com>
2025-05-14 21:15:11 +0900

    [Edit] converter.js https -> hxxp로 잘못 치환하는 로직 변경경

--- source/static/js/converter.js
+++ source/static/js/converter.js
@@ -11,19 +11,22 @@ function convertText() {
 
   let output = input.split('\n').map(line => {
     if (reverse) {
-      // 역방향 변환 (hxxps -> https, [.] -> .)
+      // 역방향 변환 (hxxps -> https, hxxp -> http, [.] -> .)
       return line
-        .replace(/hxxps?/gi, 'http')
+        .replace(/hxxps/gi, 'https')
+        .replace(/hxxp/gi, 'http')
         .replace(/\[\.\]/g, '.')
-        .replace(/\[(\d{1,3})\]\.(\d{1,3})\[(\d{1,3})\]\.(\d{1,3})/g, '$1.$2.$3.$4'); // IP 주소 역방향
+        .replace(/(\d{1,3})\[\.\](\d{1,3})\[\.\](\d{1,3})\[\.\](\d{1,3})/g, '$1.$2.$3.$4'); // IP 주소 역방향
     } else {
-      // 정방향 변환 (https -> hxxps, . -> [.] )
+      // 정방향 변환 (https -> hxxps, http -> hxxp, . -> [.] )
       return line
-        .replace(/https?/gi, 'hxxp')
-        .replace(/\./g, '[.]')
-        .replace(/(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})/g, '$1[.]$2[.]$3[.]$4'); // IP 주소 변환
+        .replace(/https/gi, 'hxxps')
+        .replace(/http/gi, 'hxxp')
+        .replace(/(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})/g, '$1[.]$2[.]$3[.]$4') // IP 주소 변환
+        .replace(/\./g, '[.]'); // 나머지 점 변환
     }
   }).join('\n');
+
 
   outputArea.value = output;
 }
